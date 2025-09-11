fd1abc4571d0bfc3a9ab3b37308c520dc4d37b0b
Ambient.Impact <i@ambientimpact.com>
2025-09-11 03:49:02 -0400

    Issue #8: SoundPack now builds sound file paths.

--- AmbientImpactItemFilter/packages/poe_sound_mix/poe_sound_mix/SoundPack.py
+++ AmbientImpactItemFilter/packages/poe_sound_mix/poe_sound_mix/SoundPack.py
@@ -1,8 +1,15 @@
+from pathlib import PurePath
+
 class SoundPack:
 
   def __init__(self, name: str, packData: dict):
 
     self._name = name
+
+    for soundName, soundFile in packData['sounds'].items():
+
+      # Build the path for each sound file.
+      packData['sounds'][soundName] = str(PurePath(packData['path'], soundFile))
 
     self._packData = packData
 
