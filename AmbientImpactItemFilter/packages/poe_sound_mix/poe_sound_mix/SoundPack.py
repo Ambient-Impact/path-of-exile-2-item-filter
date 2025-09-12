d34cc15b0dcd516f671dbd820a079e159a016756
Ambient.Impact <i@ambientimpact.com>
2025-09-12 04:48:37 -0400

    Issue #8: SoundPack now replaces tier scheme sound names with files.

--- AmbientImpactItemFilter/packages/poe_sound_mix/poe_sound_mix/SoundPack.py
+++ AmbientImpactItemFilter/packages/poe_sound_mix/poe_sound_mix/SoundPack.py
@@ -6,12 +6,44 @@ class SoundPack:
 
     self._name = name
 
+    self._packData = packData
+
+    self._buildSoundPaths()
+
+    self._buildTieredSchemes()
+
+  def _buildSoundPaths(self) -> None:
+
+    packData = self._packData
+
     for soundName, soundFile in packData['sounds'].items():
 
       # Build the path for each sound file.
       packData['sounds'][soundName] = str(PurePath(packData['path'], soundFile))
 
-    self._packData = packData
+  def _buildTieredSchemes(self) -> None:
+
+    packData = self._packData
+
+    if ('tieredSchemes' not in packData):
+
+      return
+
+    sounds = packData['sounds']
+
+    schemes = packData['tieredSchemes']
+
+    for schemeName, scheme in schemes.items():
+
+      for tierName, soundName in scheme.items():
+
+        if (soundName not in sounds):
+
+          raise Exception(
+            f'Sound "{soundName}" doesn\'t exist in the "{self._name}" sound pack.',
+          )
+
+        scheme[tierName] = sounds[soundName]
 
   @property
   def dict(self) -> dict:
